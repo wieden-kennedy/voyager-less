7544672546de78890f88ca9429ce8b62a5f96b82
David Glivar <davidglivar@gmail.com>
2014-09-23 10:47:07 -0700

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+var less = require('gulp-less')
+  , plumber = require('gulp-plumber');
+
+module.exports = function (voyager) {
+  
+  voyager.task('write', 'styles', function (done) {
+    this.src('stylesheets/main.less')
+      .pipe(plumber())
+      .pipe(less())
+      .pipe(this.out('stylesheets'))
+      .on('end', done);
+  });
+
+  voyager.task('write', 'styles-vendor', function (done) {
+    this.src('stylesheets/vendor/**')
+      .pipe(this.out('stylesheets'))
+      .on('end', done);
+  });
+
+  voyager.cancelWatch('stylesheets/**/*.css');
+  voyager.watch(['stylesheets/**/*.less', '!stylesheets/vendor/**'], 'styles');
+};
