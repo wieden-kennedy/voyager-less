0e9d1580bf0d624431f042197746ebae702c01a9
David Glivar <davidglivar@gmail.com>
2014-09-23 13:13:03 -0700

    added csso, updated task

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var less = require('gulp-less')
+var csso = require('gulp-csso')
+  , less = require('gulp-less')
   , plumber = require('gulp-plumber');
 
 module.exports = function (voyager) {
@@ -13,7 +14,20 @@ module.exports = function (voyager) {
 
   voyager.task('write', 'styles-vendor', function (done) {
     this.src('stylesheets/vendor/**')
+      .pipe(this.out('stylesheets/vendor'))
+      .on('end', done);
+  });
+
+  voyager.task('build', 'styles', function (done) {
+    this.src(['stylesheets/**/*.css', '!stylesheets/vendor/**'])
+      .pipe(csso())
       .pipe(this.out('stylesheets'))
+      .on('end', done);
+  });
+
+  voyager.task('build', 'styles-vendor', function (done) {
+    this.src('stylesheets/vendor/**')
+      .pipe(this.out('stylesheets/vendor'))
       .on('end', done);
   });
 
